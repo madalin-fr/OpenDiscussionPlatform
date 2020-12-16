02a10c8c4f6d435e48c7cc2c8f4eaa17b63b72cb
ursawarlord <francu.madalin01@yahoo.ro>
2020-12-16 14:01:40 +0200

    Change admin credentials

--- OpenDiscussionPlatform/Startup.cs
+++ OpenDiscussionPlatform/Startup.cs
@@ -36,13 +36,24 @@ namespace OpenDiscussionPlatform
 
                 // se adauga utilizatorul administrator
                 var user = new ApplicationUser();
-                user.UserName = "admin@gmail.com";
-                user.Email = "admin@gmail.com";
+                user.UserName = "mateidinescu@gmail.com";
+                user.Email = "mateidinescu@gmail.com";
                 var adminCreated = UserManager.Create(user, "!1Admin");
                 if (adminCreated.Succeeded)
                 {
                     UserManager.AddToRole(user.Id, "Admin");
                 }
+
+                var user2 = new ApplicationUser();
+                user2.UserName = "madalinfrincu@gmail.com";
+                user2.Email = "madalinfrincu@gmail.com";
+                var adminCreated2 = UserManager.Create(user2, "!1Admin");
+                if (adminCreated2.Succeeded)
+                {
+                    UserManager.AddToRole(user2.Id, "Admin");
+                }
+
+
             }
             if (!roleManager.RoleExists("Editor"))
             {
